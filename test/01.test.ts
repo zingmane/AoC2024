e083efa2748492af48ce2dc9cbb5240cd145c674
Manfred Zingl <mz@campudus.com>
2024-12-01 23:17:20 +0100

    prepare for day one

--- test/01.test.ts
+++ test/01.test.ts
@@ -1,21 +1,13 @@
 import * as day01 from "../src/01.ts";
 import { assertEquals } from "@std/assert";
 
-const input = `1000
-2000
-3000
-
-4000
-
-5000
-6000
-
-7000
-8000
-9000
-
-10000`;
+const input = `3   4
+4   3
+2   5
+1   3
+3   9
+3   3`;
 
 Deno.test("day 01", () => {
-  assertEquals(day01.run(input), [24000, 45000]);
+  assertEquals(day01.run(input), [11, "?"]);
 });
