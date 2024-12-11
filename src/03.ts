00c3aed6112ec3c1236fb381de603b8023d2412d
Manfred Zingl <mz@campudus.com>
2024-12-11 15:49:05 +0100

    day 3, part 2

--- src/03.ts
+++ src/03.ts
@@ -4,7 +4,32 @@ export const run = (raw: string) => {
 
   const part1 = parsedValidInstructions.reduce((acc, [num1, num2]) => acc + num1 * num2, 0);
 
-  const part2 = "part2";
+  const getPart2 = () => {
+    const parsedValidInstructionsPart2 = raw.match(/mul\(\d*,\d*\)|do\(\)|don't\(\)/mg)
+      ?.map((res) => {
+        if (res === "do()") {
+          return true;
+        } else if (res === "don't()") {
+          return false;
+        }
+        return res.match(/\d+/g)?.map((num) => parseInt(num, 10)) as [number, number];
+      }) as [number, number][] | boolean[];
+
+    let isInstructionEnabled = true;
+
+    return parsedValidInstructionsPart2.reduce((acc, instruction) => {
+      if (typeof instruction === "boolean") {
+        isInstructionEnabled = instruction;
+        return acc;
+      }
+      if (isInstructionEnabled) {
+        return acc + instruction[0] * instruction[1];
+      }
+      return acc;
+    }, 0);
+  };
+
+  const part2 = getPart2();
 
   return [part1, part2];
 };
--- test/03.test.ts
+++ test/03.test.ts
@@ -1,8 +1,8 @@
 import * as theDay from "../src/03.ts";
 import { assertEquals } from "@std/assert";
 
-const input = `xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))`;
+const input1 = `xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))`;
 
 Deno.test("day 03", () => {
-  assertEquals(theDay.run(input), [161, "part2"]);
+  assertEquals(theDay.run(input1), [161, 48]);
 });
